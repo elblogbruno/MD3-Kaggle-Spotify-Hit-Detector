2308fc020f4083ac675446fd5b8f87639e1a5134
Bruno Moya <elblogdebruno@outlook.es>
2021-12-06 23:39:01 +0100

    fix: bugs

--- model_test.py
+++ model_test.py
@@ -4,7 +4,11 @@ from src.score_model import *
 import joblib
 
 dataset_train, dataset_test, dataset_validate, dataset_full = generate_dataset(type='non-nlp')
-x_train, x_test, y_train, y_test, y_pred, RF = train_model(dataset_train, dataset_test, dataset_validate)
+
+x_train, x_test, y_train, y_test = get_train_test(dataset_train, dataset_test, dataset_validate)
+
+RF, y_pred = train_model(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, model_name='RF')
+
 accuracy, precision, recall, f1 = evaluate_model(x_train, x_test, y_train, y_test, y_pred, RF)
 
 filename = 'deploy/model-spotify.sav'
--- src/train_model.py
+++ src/train_model.py
@@ -10,21 +10,32 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import cross_val_predict, train_test_split, ParameterGrid
 from sklearn.preprocessing import StandardScaler, Normalizer, RobustScaler
 
+def split_dataset(dataset):
+    """ split dataset into train, test, validate """
+    dataset_train, dataset_test, dataset_validate = train_test_split(dataset, test_size=0.2, random_state=0)
+    return dataset_train, dataset_test, dataset_validate
 
-def train_model(dataset_train, dataset_test, dataset_validate):
-    """ retrain model with new song """
+def get_train_test(dataset_train, dataset_test, dataset_validate):
     x_train = dataset_train.drop(['target'], 1).values  # values converts it into a numpy array
     y_train = dataset_train['target'].values.ravel()  # -1 means that calculate the dimension of rows, but have 1 column
 
     x_test = dataset_test.drop(['target'], 1).values  # values converts it into a numpy array
     y_test = dataset_test['target'].values.ravel()  # -1 means that calculate the dimension of rows, but have 1 column
+    
+    return x_train, x_test, y_train, y_test
 
+def train_model(x_train, y_train, x_test, y_test, model_name, params=None):
+    """ retrain model with new song """
     sc =  StandardScaler()
     x_train = sc.fit_transform(x_train)
     x_test = sc.transform(x_test)
+    
+    if params:
+        RF = RandomForestClassifier(**params)
+    else:
+        RF = RandomForestClassifier()
 
-    RF = RandomForestClassifier()
     RF.fit(x_train, y_train)
     y_pred = RF.predict(x_test)
 
-    return x_train, x_test, y_train, y_test, y_pred, RF
+    return RF, y_pred
